6437b3a7b1cfc14d6d23272b7f7bd00ee5bd0054
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2020-05-10 12:15:07 +0200

    Add concrete factory class

---
+++ src/Factory/Factory.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Factory Pattern Base Class
+ * @package     Ticaje_Contract
+ * @author      Max Demian <ticaje@filetea.me>
+ */
+
+namespace Ticaje\Contract\Factory;
+
+/**
+ * Class Factory
+ * @package Ticaje\Contract\Factory
+ */
+final class Factory extends Base implements FactoryInterface
+{
+}
