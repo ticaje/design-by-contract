2e71cffd94e93b6e70b45fc12a3dd1337dc8cc4a
Hector Luis Barrientos Margolles <ticaje@filetea.me>
2020-12-03 13:55:00 +0100

    Add singleton coverage

---
+++ src/Traits/Singleton.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Decorator Implementation
+ * @category    Ticaje
+ * @author      Max Demian <ticaje@filetea.me>
+ */
+
+namespace Ticaje\Contract\Traits;
+
+/**
+ * Trait Singleton
+ * @package Ticaje\Contract\Traits
+ * This trait accomplishes singleton pattern to those classes that require to momentarily cache a result based on current execution thread.
+ */
+trait Singleton
+{
+    /**
+     * @param $binder // The binder is a class property, preferably a protected/private one
+     * @param callable $logic
+     * @return mixed
+     */
+    protected function singleton($binder, callable $logic)
+    {
+        if (!$this->{$binder}) {
+            $this->{$binder} = $logic();
+        }
+        return $this->{$binder};
+    }
+}
